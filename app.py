aacea3c3e91f11e09335950f1850cde5006bcb59
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-06-04 18:51:26 +0530

    data is CSV now

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import Flask,render_template, jsonify, request
 import random
 import json
 import data as dt
+import pandas as pd 
 
 
 app  = Flask(__name__)
@@ -17,11 +18,18 @@ PORT = 3000
 @app.route("/", methods=["GET","POST"])
 def startpy():
     
-    pnl = dt.getpnl()
+    pnl_list = get_data()
 
-    return render_template("date.html") 
+    return render_template("date.html",pnl_list=pnl_list) 
 
 
+def get_data():
+    
+    df = pd.read_csv('pnl.csv')
+
+    pnl_list    = df['pnl'].tolist()
+
+    return pnl_list
 
 if __name__ == "__main__":
     app.run(debug = True,host="0.0.0.0",port = PORT)
